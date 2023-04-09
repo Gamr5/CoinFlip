d1906c30d0400df313e6e2e5010f3309fdb4b1b1
Gamr5 <103077312+Gamr5@users.noreply.github.com>
2023-04-09 13:58:51 -0700

    multi run unfinnished
    multi run is unfinished, so i just made it so it doesn't work yet

--- scripts.js
+++ scripts.js
@@ -1,39 +1,40 @@
 var current = -1; //current value of the coinflip
 let states = [0, 1]; //heads is 0, tails is 1 (using numbers makes it easier to manage)
-var streak = 0 //current streak
-var streakType = -1 //state of streak
-var result = document.getElementById("output")
-var last = -1
-var tranlatedType //translates steak type into english, instead of numbers.
+var streak = 0; //current streak
+var streakType = -1; //state of streak
+var result = document.getElementById("output");
+var last = -1;
+var tranlatedType; //translates steak type into english, instead of numbers.
+var maxStreak = 0
 
 function run() {
+  current = states[Math.floor(Math.random() * states.length)];
 
-current = states[Math.floor(Math.random() * states.length)];
-
-
-
-
-if (streak > 0) { //if the coin has already been fliped
-    if (streakType == current) { //if current is continuing the streak, add 1 to streak
-        streak = streak  + 1
+  if (streak > 0) {
+    //if the coin has already been fliped
+    if (streakType == current) {
+      //if current is continuing the streak, add 1 to streak
+      streak = streak + 1;
     }
-if (streakType != current) {
-        streakType = current
-        streak = 0
+    if (streakType != current) {
+      streakType = current;
+      streak = 0;
     }
+  }
+
+  if (streak == 0) {
+    streakType = current;
+    streak = streak + 1;
+  }
+
+  if (current == 0) {
+    tranlatedType = "heads";
+  } else if (current == 1) {
+    tranlatedType = "tails";
+  }
+
+  result.innerHTML = `Last coin flip : ${tranlatedType}<br>Current streak: ${streak}`;
 }
-
-if (streak == 0) {
-    streakType = current
-    streak = streak + 1
-    }
-    
-
-if (current == 0) {
-    tranlatedType = "heads"
-} else if (current == 1) {
-    tranlatedType = "tails"
-}
-
-result.innerHTML = `Last coin flip : ${tranlatedType}<br>Current streak: ${streak}`
+function multiRun() {
+alert("Not funtional yet. If you want to have it added faster, code it yourself")
 }
